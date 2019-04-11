397e73861c1c08a271dc3f0839d63019042eb044
Fernando Valdes <fernandoluis.valdes@bbva.com>
2019-04-11 16:23:40 -0300

    base para nivel 2

--- src/main/java/ar/com/bbva/taller/app/endpoint/CustomerController.java
+++ src/main/java/ar/com/bbva/taller/app/endpoint/CustomerController.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RestController
@@ -19,7 +21,7 @@ public class CustomerController {
     private CustomerRepository customerRepository;
 
     @RequestMapping(method=RequestMethod.POST)
-    public @ResponseBody ResponseEntity save(@RequestBody Customer customer){
+    public @ResponseBody ResponseEntity save(@RequestBody Customer customer, HttpServletRequest request){
         return ResponseEntity.ok(customerRepository.save(customer));
     }
 
@@ -29,14 +31,14 @@ public class CustomerController {
     }
 
     @RequestMapping(method=RequestMethod.GET)
-    public @ResponseBody ResponseEntity all() {
+    public @ResponseBody ResponseEntity<Iterable<Customer>> all() {
         return ResponseEntity.ok(customerRepository.findAll());
     }
 
     @RequestMapping(value = "/{id}", method=RequestMethod.DELETE)
     public ResponseEntity delete(@PathVariable long id) {
         customerRepository.deleteById(id);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.noContent().build();
     }
 
 }
